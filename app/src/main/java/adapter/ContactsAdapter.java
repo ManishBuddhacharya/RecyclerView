e64298d08a9b862660f912631ac80c238ae320dd
ManishShubhu <lifestartedfrom1997@gmail.com>
2019-04-18 18:40:40 +0545

    Recycler view completed

--- app/src/main/java/adapter/ContactsAdapter.java
+++ app/src/main/java/adapter/ContactsAdapter.java
@@ -3,6 +3,7 @@ package adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -26,17 +27,22 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.Contac
     @NonNull
     @Override
     public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.contact, viewGroup, false);
+        return new ContactsViewHolder(view);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull ContactsViewHolder contactsViewHolder, int i) {
-
+        Contacts contact = contactLists.get(i);
+        contactsViewHolder.imgProfile.setImageResource(contact.getImageId());
+        contactsViewHolder.tvName.setText(contact.getName());
+        contactsViewHolder.tvContact.setText(contact.getContact());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return contactLists.size();
     }
 
     public class ContactsViewHolder extends RecyclerView.ViewHolder{
--- app/src/main/java/np/com/softwarica/recyclerview/MainActivity.java
+++ app/src/main/java/np/com/softwarica/recyclerview/MainActivity.java
@@ -2,12 +2,39 @@ package np.com.softwarica.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.ContactsAdapter;
+import modal.Contacts;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvContact;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvContact = findViewById(R.id.rvContact);
+        List<Contacts> contactList = new ArrayList<>();
+        contactList.add(new Contacts("Batman","9806151615", R.drawable.batman));
+        contactList.add(new Contacts("Ironman","9806151615", R.drawable.ironman));
+        contactList.add(new Contacts("Hulk","9806151612", R.drawable.hulk));
+        contactList.add(new Contacts("Captain Marvel","9806151615", R.drawable.cm));
+        contactList.add(new Contacts("Black Panther","9806151615", R.drawable.bp));
+        contactList.add(new Contacts("Thor","9806151615", R.drawable.thor));
+        contactList.add(new Contacts("Captain America","9806151615", R.drawable.ca));
+        contactList.add(new Contacts("Spiderman","9806151615", R.drawable.spiderman));
+        contactList.add(new Contacts("Loki","9806151615", R.drawable.loki));
+        contactList.add(new Contacts("Black Widow","9806151615", R.drawable.bw));
+
+        ContactsAdapter contactsAdapter = new ContactsAdapter(this, contactList);
+        rvContact.setAdapter(contactsAdapter);
+        rvContact.setLayoutManager(new LinearLayoutManager(this));
     }
 }
